12c7d03c9ab7f5f06789b89e7c02c0416f141ff8
Gianmarco <72602391+Gianmarc0Dev@users.noreply.github.com>
2021-08-13 16:10:34 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -11,7 +11,7 @@ async def say(ctx, word):#-----------------------------------the name of the fun
 
 @bot.command(name="your command name")#-----------------------if you want a different name for the command use the parameter name
 async def example_function(ctx):#----------------------------and call the function with the name you want
-  continue
+  return
   
 @bot.event
 async def on_message(message):#------------------------------this function is called when a message is sent in one of the bot servers
